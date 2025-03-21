6e5271ba73fa30f2519daeead963dc4345e33f22
otelhan <otelhan@gmail.com>
2025-03-21 00:39:05 -0400

    Removed the message wrapping - send the data packet as-is Fixed config access (was using non-existent config_handler) Added debug print for connection URI

--- src/networking/controller_node.py
+++ src/networking/controller_node.py
@@ -312,21 +312,17 @@ class ControllerNode:
 
     async def send_data_to(self, target_name, data):
         """Send data to another controller"""
-        target = self.config_handler.config['controllers'].get(target_name)
+        target = self.config['controllers'].get(target_name)  # Use self.config directly
         if not target:
             print(f"Unknown target controller: {target_name}")
             return False
             
         try:
             uri = f"ws://{target['ip']}:{self.port}"
+            print(f"Sending to {uri}")
             async with websockets.connect(uri) as websocket:
-                message = {
-                    "type": "data",
-                    "source": self.controller_name,
-                    "data": data,
-                    "timestamp": time.time()
-                }
-                await websocket.send(json.dumps(message))
+                # Send the data packet as-is, maintaining the format
+                await websocket.send(json.dumps(data))
                 response = await websocket.recv()
                 print(f"Response from {target_name}: {response}")
                 return True
