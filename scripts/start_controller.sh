f0dcedb2ec0c940baa2a364ef1e7021e77f53be0
otelhan <otelhan@gmail.com>
2025-04-10 22:12:57 -0400

    fixed startup_controller.sh

--- scripts/start_controller.sh
+++ scripts/start_controller.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Source virtual environment
-source /home/pi/venice/venv/bin/activate
+# Set path to virtual environment
+VENV=/home/pi/venice/venv
+
+# Activate virtual environment
+export PYTHONPATH=/home/pi/venice
+source $VENV/bin/activate
 
 # Change to project directory
 cd /home/pi/venice
 
 # Run controller with node name argument
-python -m src.run_controller $1 
+$VENV/bin/python -m src.run_controller $1 
